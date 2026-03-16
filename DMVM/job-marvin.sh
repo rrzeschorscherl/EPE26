cfd68e68a95833b8d2512928963d35e556c4d2ac
Georg Hager <georg.hager@fau.de>
2026-03-16 11:29:15 +0100

    Fixed no outer unroll problem

--- DMVM/job-marvin.sh
+++ DMVM/job-marvin.sh
@@ -18,10 +18,10 @@ icx -Ofast -xHost -qopt-zmm-usage=high -o ./dmvm.exe C/dmvm.c
 # choose between C and Fortran
 #icx -DLIKWID_PERFMON -Ofast -xHost -qopt-zmm-usage=high -o ./dmvm.exe C/dmvm-marker.c -llikwid
 #ifx -Ofast -xHost -qopt-zmm-usage=high -o ./dmvm.exe F90/dmvm-marker.f90 -llikwid
-#gcc -DLIKWID_PERFMON -mcmodel=large -Ofast -march=icelake-server -mprefer-vector-width=512 -o ./dmvm.exe C/dmvm-marker.c -llikwid
-gfortran -I/usr/include -Ofast -mcmodel=large -march=icelake-server -mprefer-vector-width=512 -o ./dmvm.exe F90/dmvm-marker.f90 -llikwid
+gcc -DLIKWID_PERFMON -mcmodel=large -Ofast -march=icelake-server -mprefer-vector-width=512 -o ./dmvm.exe C/dmvm-marker.c -llikwid
+#gfortran -I/usr/include -Ofast -mcmodel=large -march=icelake-server -mprefer-vector-width=512 -o ./dmvm.exe F90/dmvm-marker.f90 -llikwid
 
-likwid-perfctr -g FLOPS_DP -m -C 0 ./dmvm.exe 10000 10000
+srun --cpu-bind=none --cpu-freq=2000000-2000000:performance likwid-perfctr -g FLOPS_DP -m -C 0 ./dmvm.exe 10000 10000
 
 #exit
 
